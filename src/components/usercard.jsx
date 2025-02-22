e31c39e242849a54ce33bef8451e5f7644fa2491
Avishkar-s73 <avishkar.durgade.s73@kalvium.community>
2025-02-22 09:34:51 +0530

    bugfix

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -4,7 +4,7 @@ import "./Usercard.css";
 const Usercard = () => {
   const profilePhoto =
     "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250";
-  const name = "John Doe";
+  const name = "Avishkar";
   const email = "avishkar@example.com";
   const phone = "+91 1234567890";
   const address = "Kalvium MIT";
